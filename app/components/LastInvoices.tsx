b48a72bc93f157c07b2295d6b486f26f3388ab3f
Eduardo Santos <eduluis.cco@gmail.com>
2024-05-30 01:09:44 -0300

    adjust tooltip

--- app/components/LastInvoices.tsx
+++ app/components/LastInvoices.tsx
@@ -20,27 +20,25 @@ interface Props {}
 const LastInvoices: NextPage<Props> = ({}) => {
   return (
     <div className="flex w-full justify-center">
-        <Carousel className="container">
-          <CarouselContent >
-            <CarouselItem>        
-              <LastInvoice titulo={'Nota fiscal'} valor={'2.400'} number={'0001'}/>
-            </CarouselItem>
-            <CarouselItem>
-              <LastInvoice titulo={'Nota fiscal'} valor={'25.000'} number={'0002'}/>        
-            </CarouselItem>
-          </CarouselContent>
-          <TooltipProvider delayDuration={100}>
-            <Tooltip>
-              <TooltipTrigger>
-                <SwipeLeftIcon className='text-indigo-950 absolute bottom-20 left-0'/>
-                <SwipeRightIcon className='text-indigo-950 absolute bottom-20 right-0'/>
-              </TooltipTrigger>
-              <TooltipContent className='relative left-[32rem] bottom-36'>
-                <p>Arraste</p>
-              </TooltipContent>
-            </Tooltip>
-           </TooltipProvider>
-        </Carousel>
+       <TooltipProvider delayDuration={100} >
+          <Tooltip>
+            <TooltipTrigger className="flex w-full justify-center">
+              <Carousel className="container">
+                <CarouselContent >
+                  <CarouselItem>        
+                    <LastInvoice titulo={'Nota fiscal'} valor={'2.400'} number={'0001'}/>
+                  </CarouselItem>
+                  <CarouselItem>
+                    <LastInvoice titulo={'Nota fiscal'} valor={'25.000'} number={'0002'}/>        
+                  </CarouselItem>
+                </CarouselContent>
+              </Carousel>
+            </TooltipTrigger>
+            <TooltipContent>
+              <SwipeRightIcon className='text-indigo-950'/>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
     </div>  
     )
 }
